05a5d2db052492739af466e06525af0d5d8d676f
annisarzkta <annisarizkyta14@gmail.com>
2023-07-14 23:40:51 +0700

    menambahkan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:uts_aplikasibergerak/screens/splash_screen/splash_screen.dart';
 import 'package:uts_aplikasibergerak/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
